45dfc194a0a0e3464c24a078a47cdb99d39f9fea
fnduald <qwer12347035~>
2026-03-19 14:58:26 +0900

    Add:플레이어 기본 움직임

--- GameEngineIntro_C/Assets/PlayController.cs
+++ GameEngineIntro_C/Assets/PlayController.cs
@@ -1,27 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
     void Update()
     {
+        // 캐릭터 좌우 반전 처리
         if (moveInput.x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
-
         }
         else if (moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
-
         }
-        transform.Translate(Vector3.right * moveInput.x * Time.deltaTime);
+
+        // 애니메이션 파라미터 제어
+        if (moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+
+        // 실제 이동 처리
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
-    }
+}
